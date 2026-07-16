6c6345c0efdc0c848e445c5a72b74db2cbf07a8d
Carlavier <namto10042004@gmail.com>
2026-07-16 12:40:45 +0700

    modify formatter

--- private_dot_config/nvim/lua/plugins/guess-indent.lua
+++ private_dot_config/nvim/lua/plugins/guess-indent.lua
@@ -2,6 +2,14 @@ return {
   "NMAC427/guess-indent.nvim",
   event = { "BufReadPost", "BufNewFile" },
   config = function()
-    require("guess-indent").setup({})
+    require("guess-indent").setup({
+      auto_cmd = true,
+      filetype_blocklist = {
+        "cpp",
+        "c",
+        "hpp",
+        "h",
+      },
+    })
   end,
 }
